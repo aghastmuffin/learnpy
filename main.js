142f101e3585dff2e9fc3981505533c3124b2ef3
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-04-01 20:14:51 -0700

    added some testing

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 async function main(){
     let pyodide = await loadPyodide();
+    console.log('ran')
     resp = pyodide.runPython(document.getElementById("inpt").value);
     console.log(resp)
     document.getElementById("resptxt").textContent = resp;
 }
+console.log("init");
