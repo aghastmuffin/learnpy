3e47ce5a0ef172445b6e0853d44ddf2d694a2e44
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-04-01 20:11:36 -0700

    getting to know pyodide

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+async function main(){
+    let pyodide = await loadPyodide();
+    resp = pyodide.runPython(document.getElementById("inpt").value);
+    console.log(resp)
+    document.getElementById("resptxt").textContent = resp;
+}
